48e87871bc84e7efa1999f64601d3dbbb77ae2d1
Sergey Lazurenko <s.lazurenko@trucker.group>
2025-11-05 15:37:39 +0300

    Add docker-compose for postgres

--- configs/db_config.go
+++ configs/db_config.go
@@ -1,6 +1,11 @@
 package configs
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type DBConfig struct {
 	Host     string
@@ -12,12 +17,17 @@ type DBConfig struct {
 }
 
 func InitDatabaseConfig() *DBConfig {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	return &DBConfig{
 		Host:     os.Getenv("POSTGRES_HOST"),
 		Port:     os.Getenv("POSTGRES_PORT"),
-		Username: os.Getenv("POSTGRES_USERNAME"),
+		Username: os.Getenv("POSTGRES_USER"),
 		Password: os.Getenv("POSTGRES_PASSWORD"),
-		DBName:   os.Getenv("POSTGRES_DB_NAME"),
-		SSLMode:  os.Getenv("POSTGRES_SSL_MODE"),
+		DBName:   os.Getenv("POSTGRES_DB"),
+		SSLMode:  os.Getenv("POSTGRES_SSLMODE"),
 	}
 }
